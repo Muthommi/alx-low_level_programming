c8c03964f1932c54a9b721191d4e0753d7460be6
Muthommi <kelvinmartinmuthomi@gmail.com>
2023-09-14 03:08:53 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	int i;
+	char j;
 
-	while (i <= 0)
+	for (i = 0 ; i < 10 ; i++)
 	{
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-	_putchar(c);
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
 	}
 	_putchar('\n');
-	i++;
-	}
 }
