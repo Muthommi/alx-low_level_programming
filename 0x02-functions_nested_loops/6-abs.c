577fefd48ce7fef9f3503e1544e7cbe211499113
Muthommi <kelvinmartinmuthomi@gmail.com>
2023-10-04 06:14:25 -0700

    Abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
-/**
- * _abs - main function
- * Return: always 0
- */
 
-int _abs(int)
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
