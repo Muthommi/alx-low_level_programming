fcbdc53f67d41c8d32da55c29bc2a532ae7e49c0
Muthommi <kelvinmartinmuthomi16@gmail.com>
2023-11-22 22:10:23 -0800

    My new name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the name, followed by a new line
+ *
+ * @argc: count of arguments
+ * @argv: array containing command line arguments
+ *
+ * Return: (0) when successful
+ */
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
