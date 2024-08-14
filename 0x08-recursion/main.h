0c89a911d4c5c6bfa09e2588b3b40d784a03d458
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-08-14 11:09:17 +0300

    The way we only use a fraction of our brains true potential

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,8 @@ int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int i);
 int is_prime_number(int n);
 int check_prime(int n, int i);
+int check_palindrome(char *s, int start, int end);
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN_H */
